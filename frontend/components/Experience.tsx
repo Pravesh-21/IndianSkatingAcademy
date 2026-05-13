c09599b33503e1a638497c505647d11b752cd9e2
Pravesh Shrivastava <praveshpshrivastava@gmail.com>
2026-05-13 19:40:28 +0530

    Experience.tsx update

--- frontend/components/Experience.tsx
+++ frontend/components/Experience.tsx
@@ -165,7 +165,7 @@ function IntegratedSkateWheel({ scrollRef }: { scrollRef: RefObject<{ progress: 
       {/* Bearing glow ring */}
       <mesh position={[0, 0, 0]}>
         <torusGeometry args={[1.35, 0.02, 8, 64]} />
-        <meshBasicMaterial color="#00C2FF" emissive="#00C2FF" emissiveIntensity={0.5} />
+        <meshStandardMaterial color="#00C2FF" emissive="#00C2FF" emissiveIntensity={0.5} />
       </mesh>
       
       {/* Center bearing hub */}
